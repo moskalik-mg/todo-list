d8aca8af1c7348cc2fcd5dc76051875eecf4120a
moskalik-mg <moskalik.mg@gmail.com>
2020-02-10 19:58:11 +0300

    show Modal edit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-const input = document.querySelector('#task')  //input
-const buttonAdd = document.querySelector('#addTask')  // button add
+const input = document.querySelector('#task') //input
+const buttonAdd = document.querySelector('#addTask') // button add
 const KEY_LOCAL_STORAGE = 'arrayTasks'
 const arrayTasks = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE)) || []
 const todoList = document.querySelector('.todo-list')
+const editTaskInput = document.querySelector('#editTaskInput')
+const buttonSaveChanges = document.querySelector('#saveChanges')
+
 
 render(arrayTasks);
 
 input.addEventListener('input', () => {
     let valueInput = input.value
-    if(valueInput < 1){
+    if (valueInput < 1) {
         buttonAdd.setAttribute('disabled', 'disabled')
-    }
-    else{
+    } else {
         buttonAdd.removeAttribute('disabled')
     }
-    
+
 });
 
 buttonAdd.addEventListener('click', (e) => {
@@ -22,22 +24,25 @@ buttonAdd.addEventListener('click', (e) => {
     addTask()
 });
 
-function createList(name){
-    return {id: Date.now().toString(), task: name}
+function createList(name) {
+    return {
+        id: Date.now().toString(),
+        task: name
+    }
 }
 
-function saveToStorage(array){
+function saveToStorage(array) {
     localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(array));
 }
 
-function render(arrayTasks){
+function render(arrayTasks) {
 
     todoList.innerHTML = ''
 
     arrayTasks.forEach((element, index) => {
-        
-        const block  = 
-        `<div class="todo-item card">
+
+        const block =
+            `<div class="todo-item card">
             <div class="card-body">
                 <div class="todo-name card-title">
                     Name 1
@@ -47,43 +52,42 @@ function render(arrayTasks){
                 </div>
             </div>
             <div class="button-controls">
-                <div class="edit-card"><i class="fas fa-pencil-alt"></i></div>
+                <div class="edit-card" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-pencil-alt"></i></div>
                 <div class="remove-card"><i class="fas fa-trash-alt"></i></div>
             </div>
         </div>`;
-        
+
         todoList.insertAdjacentHTML('afterbegin', block);
 
         const clearButton = document.querySelector(".remove-card");
         clearButton.addEventListener('click', () => {
-            arrayTasks.splice(index, 1);  
+            arrayTasks.splice(index, 1);
             localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(arrayTasks));
             render(arrayTasks);
         });
-        
-    });
-    
 
+        const buttonEdit = document.querySelector('.edit-card');
+        buttonEdit.addEventListener('click', () => {
+            let arrayTasks = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE));  
+            let valueTask = arrayTasks[index].task          
+            editTaskInput.value = valueTask;
+        });
+
+    });
 }
 
-function addTask(){
+function addTask() {
     let valueInput = input.value;
-    if(valueInput < 1){
+    if (valueInput < 1) {
         alert('Хорошая попытка, но нет)');
         buttonAdd.setAttribute('disabled', 'disabled');
         return false
-    }
-    else{
+    } else {
         const list = createList(valueInput);
         arrayTasks.push(list);
         saveToStorage(arrayTasks);
         render(arrayTasks)
         input.value = '';
         buttonAdd.setAttribute('disabled', 'disabled');
-    }    
+    }
 }
-
-
-
-
-
